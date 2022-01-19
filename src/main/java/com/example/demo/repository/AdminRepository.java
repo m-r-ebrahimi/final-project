effa9d0ed909864ee0bb39afac30042fff2e6dd1
m-r-ebrahimi <emohammadreza568@gmail.com>
2022-01-19 19:09:25 +0330

    add user repository

--- src/main/java/com/example/demo/repository/AdminRepository.java
+++ src/main/java/com/example/demo/repository/AdminRepository.java
@@ -2,9 +2,13 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.users.Admin;
 import com.example.demo.repository.core.BaseRepository;
+import org.springframework.data.domain.Page;
 
+import java.awt.print.Pageable;
 import java.util.Optional;
 
 public interface AdminRepository extends BaseRepository<Admin> {
     Optional<Admin> findById(Long id);
+    Optional<Admin> findByUsernameAndPassword(String username,String password);
+    Page<Admin> findAll(Pageable pageable);
 }
--- src/main/java/com/example/demo/repository/CustomerRepository.java
+++ src/main/java/com/example/demo/repository/CustomerRepository.java
@@ -1,8 +1,15 @@
 package com.example.demo.repository;
 
+import com.example.demo.entity.users.Admin;
 import com.example.demo.entity.users.Customer;
 import com.example.demo.repository.core.BaseRepository;
+import org.springframework.data.domain.Page;
+
+import java.awt.print.Pageable;
+import java.util.Optional;
 
 public interface CustomerRepository extends BaseRepository<Customer> {
-
+    Optional<Customer> findById(Long id);
+    Optional<Customer> findByUsernameAndPassword(String username,String password);
+    Page<Customer> findAll(Pageable pageable);
 }
