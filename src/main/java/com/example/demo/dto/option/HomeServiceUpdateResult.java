f84f5f9ac7f65b2a430fceaa07ded8fb40233b02
m-r-ebrahimi <emohammadreza568@gmail.com>
2022-01-19 17:43:37 +0330

    add lombok annotations to dto

--- src/main/java/com/example/demo/dto/option/HomeServiceUpdateResult.java
+++ src/main/java/com/example/demo/dto/option/HomeServiceUpdateResult.java
@@ -1,4 +1,13 @@
 package com.example.demo.dto.option;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class HomeServiceUpdateResult {
 }
