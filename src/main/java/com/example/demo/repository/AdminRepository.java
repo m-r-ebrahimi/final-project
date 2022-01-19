c2bd30ff57b2f0d37bb81298c8326d72ebc16a39
m-r-ebrahimi <emohammadreza568@gmail.com>
2022-01-19 18:56:20 +0330

    add user repository

--- src/main/java/com/example/demo/repository/AdminRepository.java
+++ src/main/java/com/example/demo/repository/AdminRepository.java
@@ -3,5 +3,8 @@ package com.example.demo.repository;
 import com.example.demo.entity.users.Admin;
 import com.example.demo.repository.core.BaseRepository;
 
+import java.util.Optional;
+
 public interface AdminRepository extends BaseRepository<Admin> {
+    Optional<Admin> findById(Long id);
 }
---
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.users.User;
+import com.example.demo.repository.core.BaseRepository;
+
+public interface UserRepository extends BaseRepository<User> {
+}
